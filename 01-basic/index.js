3ef7929ab1a0bd5fffb24f2517eb6008c5aa97e9
99Ace <chunliang.ang@gmail.com>
2020-09-28 05:39:46 +0000

    Created Templates

--- 01-basic/index.js
+++ 01-basic/index.js
@@ -1,12 +1,25 @@
 const express = require('express')
+const hbs = require('hbs')
 
+// 1A SET UP EXPRESS
 let app = express ();
+// 1B SET UP VIEW ENGINE
+app.set('view engine', 'hbs');
 
-// ADD ROUTES HERE
-// INDEX ROUTE
-app.get('/', function(req,res){
-    res.send("<h1>Hello World Express</h1>")
+
+
+// 2. ADD ROUTES HERE
+
+// INITIAL INDEX ROUTE
+// app.get('/', function(req,res){
+//     res.send("<h1>Hello World Express</h1>")
+// })
+
+// NEW INDEX ROUTE FOR TEMPLATE
+app.get ('/', function(req,res){
+    res.render('index');
 })
+
 
 // ADD A PARAMETERISED ROUTE
 app.get('/hello/:name', (req,res)=>{
@@ -14,7 +27,11 @@ app.get('/hello/:name', (req,res)=>{
     res.send("Hi, "+ name);
 })
 
-// 3000 refers to the POP
+// 
+
+
+
+// 3. RUN SERVER : 3000 refers to the POP
 app.listen (3000, ()=>{
     console.log('server started')
 })
